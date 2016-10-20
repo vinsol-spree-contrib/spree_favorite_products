7fd7aeff2e06dbf74c9b49de1601383c2bdfb19e
monal <monal@vinsol.com>
2016-10-20 12:47:05 +0530

    Fix CR issues

--- app/models/spree/favorite.rb
+++ app/models/spree/favorite.rb
@@ -1,9 +1,9 @@
 module Spree
   class Favorite < ActiveRecord::Base
 
-  with_options required: true do |favorite|
-    favorite.belongs_to :product
-    favorite.belongs_to :user
+  with_options required: true do
+    belongs_to :product
+    belongs_to :user
   end
 
     validates :product_id, uniqueness: { scope: :user_id, message: Spree.t(:duplicate_favorite), allow_blank: true }
