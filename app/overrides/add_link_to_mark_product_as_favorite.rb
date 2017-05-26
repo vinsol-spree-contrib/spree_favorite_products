987049659b58ac61a39e1dd84870954c1ffdc988
Anurag Bhardwaj <anuragbhardwaj.mait@gmail.com>
2017-05-26 12:53:40 +0530

    modifed existing code syntax deprications

--- app/overrides/add_link_to_mark_product_as_favorite.rb
+++ app/overrides/add_link_to_mark_product_as_favorite.rb
@@ -5,7 +5,7 @@ Deface::Override.new(
   text: %Q{
     <%= render_original %>
     <% if spree_user_signed_in? && spree_current_user.has_favorite_product?(@product.id) %>
-      <%=  link_to Spree.t(:unmark_as_favorite), favorite_product_path(id: @product.id), method: :delete, remote: true, class: 'favorite_link btn btn-primary pull-right', id: 'unmark-as-favorite' %>
+      <%= link_to Spree.t(:unmark_as_favorite), favorite_product_path(id: @product.id), method: :delete, remote: true, class: 'favorite_link btn btn-primary pull-right', id: 'unmark-as-favorite' %>
     <% else %>
       <%= link_to Spree.t(:mark_as_favorite), favorite_products_path(id: @product.id), method: :post, remote: spree_user_signed_in?, class: 'favorite_link btn btn-primary pull-right', id: 'mark-as-favorite' %>
     <% end %>
