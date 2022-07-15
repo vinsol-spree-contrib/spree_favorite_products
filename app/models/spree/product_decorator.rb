de7da6ef7151e34b75ebc91022c60d5086a7bc2a
umairrazam <omairrr.azam@gmail.com>
2022-07-15 15:52:59 +0500

    feat/favorite-product

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,8 +1,11 @@
-Spree::Product.class_eval do
-  has_many :favorites, as: :favoritable, dependent: :destroy
-  has_many :favorite_users, through: :favorites, class_name: 'Spree::User', source: :user
-
-  scope :favorite, -> { joins(:favorites).distinct }
-  scope :order_by_favorite_users_count, ->(asc = false) { order(favorite_users_count: "#{asc ? 'asc' : 'desc'}") }
-
+module Spree
+  module ProductDecorator
+	  	def self.prepended(base)
+	      base.has_many :favorites, as: :favoritable, dependent: :destroy
+	      base.has_many :favorite_users, through: :favorites, class_name: 'Spree::User', source: :user
+	      base.scope :favorite, -> { joins(:favorites).distinct }
+	      base.scope :order_by_favorite_users_count, ->(asc = false) { order(favorite_users_count: "#{asc ? 'asc' : 'desc'}") }
+	    end
+	end
 end
+::Spree::Product.prepend(Spree::ProductDecorator)
--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,13 +1,17 @@
-Spree::User.class_eval do
-  has_many :favorites, dependent: :destroy
-  has_many :favorite_products, through: :favorites, source: :favoritable, source_type: 'Spree::Product'
-  has_many :favorite_variants, through: :favorites, source: :favoritable, source_type: 'Spree::Variant'
+module Spree
+  module UserDecorator
+  		def self.prepended(base)
+		  base.has_many :favorites, dependent: :destroy
+		  base.has_many :favorite_products, through: :favorites, source: :favoritable, source_type: 'Spree::Product'
+		  base.has_many :favorite_variants, through: :favorites, source: :favoritable, source_type: 'Spree::Variant'
+		end
+		  def has_favorite_product?(product_id)
+		    favorites.exists? favoritable_id: product_id, favoritable_type: 'Spree::Product'
+		  end
 
-  def has_favorite_product?(product_id)
-    favorites.exists? favoritable_id: product_id, favoritable_type: 'Spree::Product'
-  end
-
-  def has_favorite_variant?(variant_id)
-    favorites.exists? favoritable_id: variant_id, favoritable_type: 'Spree::Variant'
-  end
+		  def has_favorite_variant?(variant_id)
+		    favorites.exists? favoritable_id: variant_id, favoritable_type: 'Spree::Variant'
+		  end
+	end
 end
+::Spree::User.prepend(Spree::UserDecorator)
